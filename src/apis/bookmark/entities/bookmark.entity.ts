7adf01873bd7580f63bb587bf625a7e58c01321d
favorsyoon <favorsyoon@gmail.com>
2023-03-03 09:44:56 +0900

    [FIX]  Bookmark_collection 테이블 추가 post 공개 비공개 컬럼 추가, user 테이블 팔로잉 팔로워 수정필요, 제거

--- src/apis/bookmark/entities/bookmark.entity.ts
+++ src/apis/bookmark/entities/bookmark.entity.ts
@@ -1,7 +1,8 @@
+import { BookmarkCollection } from 'src/apis/bookmark-collection/entities/bookmark-collection.entity';
 import { Post } from 'src/apis/post/entities/post.entity';
 import { Restaurant } from 'src/apis/restaurant/entities/restaurant.entity';
 import { User } from 'src/apis/user/entities/user.entity';
-import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, ManyToMany, JoinTable, ManyToOne } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, ManyToMany, JoinTable, ManyToOne, JoinColumn } from 'typeorm';
 
 @Entity()
 export class Bookmark {
@@ -14,19 +15,21 @@ export class Bookmark {
   @UpdateDateColumn({ name: 'updated_at' })
   updated_at: Date;
 
-  @ManyToMany(type => Restaurant, restaurants => restaurants.bookMarks)
-  @JoinTable()
-  restaurants: Restaurant[];
+  @ManyToOne(type => Restaurant, restaurant => restaurant.bookmarks)
+  @JoinColumn()
+  restaurant: Restaurant;
 
-  @ManyToMany(type => Post, post => post.bookmarks)
-  @JoinTable()
-  posts: Post[];
+  @ManyToOne(type => Post, post => post.bookmarks)
+  @JoinColumn()
+  post: Post;
 
   @ManyToOne(type => User, user => user.bookmarks)
   user: User;
 
-
-
+  @ManyToMany(type => BookmarkCollection, bookmark_collections => bookmark_collections.bookmarks)
+  @JoinTable() 
+  bookmark_collections: BookmarkCollection[];
+  //TODO: 다대다 관계에서는 @JoinTable()은 한쪽만 써야합니다.
 }
 
 
