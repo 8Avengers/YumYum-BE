5f0575f2f28eacac2bf340861800bd7bdd3823a4
sayapin1 <sayapinn1@gmail.com>
2023-03-06 17:52:39 +0900

    feat: 리뷰 포스팅 작성, 수정, 삭제, 보기 기능 0.1

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,17 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from './common/config/typeorm.config.service';
+import { PostModule } from './apis/post/post.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
-   
       useClass: TypeOrmConfigService,
-
-
     }),
 
+    PostModule,
     //TODO: 모듈 삽입, 어떤 모듈을 넣는 게 맞을까
   ],
-
 })
 export class AppModule {}
-
