20b1ff9580aaf118965b99edc111a09b2d4c3200
Admine1147 <117708597+Admine1147@users.noreply.github.com>
2023-03-08 17:40:09 +0900

    주석 추가

--- src/apis/search/search.service.ts
+++ src/apis/search/search.service.ts
@@ -60,7 +60,6 @@ export class SearchService {
       where: [{ deleted_at: null }, { name: hashtag }],
     });
 
-    console.log(postSearchByHashtagResult);
     return postSearchByHashtagResult;
   }
 }
