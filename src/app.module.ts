277910244c6f5a2804b66ad5451d381afe0d2524
pyoja <wjdgns13nr@naver.com>
2023-03-10 01:45:13 +0900

    fix: 컬렉션, 북마크 병합

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { TypeOrmConfigService } from './common/config/typeorm.config.service';
 import { PostModule } from './apis/post/post.module';
-import { CollectionModule } from './apis/bookmark-collection/bookmark-collection.module';
+import { CollectionModule } from './apis/collection/collection.module';
 import { BookmarkModule } from './apis/bookmark/bookmark.module';
 
 import { AuthModule } from './apis/auth/auth.module';
 import { UserModule } from './apis/user/user.module';
-
-
 
 @Module({
   imports: [
@@ -19,15 +17,12 @@ import { UserModule } from './apis/user/user.module';
       useClass: TypeOrmConfigService,
     }),
 
-
     PostModule,
     CollectionModule,
     BookmarkModule,
 
-
     UserModule, //
     AuthModule, //
-
   ],
 })
 export class AppModule {}
