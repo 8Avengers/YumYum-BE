3e5c87d035eb8034cdee027f6c70b2e36f934267
pyoja <wjdgns13nr@naver.com>
2023-03-10 01:01:07 +0900

    fix: 북마크, 컬렉션 병합

--- src/apis/bookmark-collection/dto/create-bookmarkCollection.dto.ts
+++
@@ -1,6 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class CreateCollectionDto {
-  @IsString()
-  readonly name: string;
-}
--- src/apis/bookmark-collection/dto/update-bookmarkCollection.dto.ts
+++
@@ -1,6 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class UpdateCollectionDto {
-  @IsString()
-  readonly name: string;
-}
