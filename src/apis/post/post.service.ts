0872d71ccd2e37b1bd9871ad18f671ca9e5ef8f9
sayapin1 <sayapinn1@gmail.com>
2023-03-10 20:54:09 +0900

    feat: 포스트 해시태그 1.0

--- src/apis/post/post.service.ts
+++ src/apis/post/post.service.ts
@@ -12,25 +12,28 @@ import { Repository } from 'typeorm';
 import { Post } from './entities/post.entity';
 import { PostLikeService } from './post-like.service';
 import { PostWithLikesDto } from './dto/post-with-likes.dto';
+import { PostHashtagService } from './post-hashtag.service';
 
 @Injectable()
 export class PostService {
   constructor(
     @InjectRepository(Post) private postRepository: Repository<Post>,
     private readonly likeService: PostLikeService,
+    private readonly postHashtagService: PostHashtagService,
   ) {}
 
   /*
-                            ### 23.03.08
-                            ### 이드보라
-                            ### 조건 없이 모든 포스팅 불러오기(뉴스피드 페이지).좋아요 기능 추가
-                            */
+                                          ### 23.03.08
+                                          ### 이드보라
+                                          ### 조건 없이 모든 포스팅 불러오기(뉴스피드 페이지).좋아요 기능 추가
+                                          */
   async getPosts(): Promise<PostWithLikesDto[]> {
     try {
       const posts = await this.postRepository
         .createQueryBuilder('post')
         .leftJoinAndSelect('post.restaurant', 'restaurant')
         .leftJoinAndSelect('post.user', 'user')
+        .leftJoinAndSelect('post.hashtags', 'hashtag')
         .where('post.deleted_at IS NULL')
         .andWhere('post.visibility = "public"')
         .select([
@@ -41,6 +44,7 @@ export class PostService {
           'post.updated_at',
           'user.nickname',
           'restaurant.name',
+          'hashtag.name',
         ])
         .getMany();
       if (!posts || posts.length === 0) {
@@ -70,16 +74,17 @@ export class PostService {
   }
 
   /*
-                              ### 23.03.08
-                              ### 이드보라
-                              ### 포스팅 상세보기.좋아요 기능 추가
-                              */
+                                            ### 23.03.10
+                                            ### 이드보라
+                                            ### 포스팅 상세보기.좋아요 기능 추가
+                                            */
   async getPostById(id: number) {
     try {
       const post = await this.postRepository
         .createQueryBuilder('post')
         .leftJoinAndSelect('post.restaurant', 'restaurant')
         .leftJoinAndSelect('post.user', 'user')
+        .leftJoinAndSelect('post.hashtags', 'hashtag')
         .where('post.id = :id', { id })
         .andWhere('post.deleted_at IS NULL')
         .andWhere('post.visibility = "public"')
@@ -90,6 +95,7 @@ export class PostService {
           'post.updated_at',
           'user.nickname',
           'restaurant.name',
+          'hashtag.name',
         ])
         .getOne();
 
@@ -113,18 +119,32 @@ export class PostService {
   }
 
   /*
-                              ### 23.03.06
-                              ### 이드보라
-                              ### 포스팅 작성
-                              */
-  createPost(content: string, rating: number, img: string, visibility) {
+                                            ### 23.03.10
+                                            ### 이드보라
+                                            ### 포스팅 작성(해시태그 추가)
+                                            */
+  async createPost(
+    content: string,
+    rating: number,
+    img: string,
+    visibility,
+    hashtagNames: string[],
+  ) {
     try {
-      return this.postRepository.insert({
+      const post = await this.postRepository.create({
         content,
         rating,
         img_url: img,
         visibility,
       });
+
+      const hashtags = await this.postHashtagService.createOrUpdateHashtags(
+        hashtagNames,
+      );
+
+      post.hashtags = hashtags;
+
+      return await this.postRepository.save(post);
     } catch (err) {
       console.error(err);
       throw new InternalServerErrorException(
@@ -134,27 +154,37 @@ export class PostService {
   }
 
   /*
-                              ### 23.03.06
-                              ### 이드보라
-                              ### 포스팅 수정
-                              */
+                                            ### 23.03.10
+                                            ### 이드보라
+                                            ### 포스팅 수정
+                                            */
   async updatePost(
     id: number,
     content: string,
     rating: number,
     img: string,
     visibility,
+    hashtagNames: string[],
   ) {
     try {
-      const result = await this.postRepository.update(id, {
+      const post = await this.postRepository.findOne({ where: { id } });
+      if (!post) {
+        throw new NotFoundException(`존재하지 않는 포스트입니다.`);
+      }
+
+      await this.postRepository.update(id, {
         content,
         rating,
         img_url: img,
         visibility,
       });
-      if (result.affected === 0) {
-        throw new NotFoundException(`Post with id ${id} not found.`);
-      }
+
+      const hashtags = await this.postHashtagService.createOrUpdateHashtags(
+        hashtagNames,
+      );
+
+      post.hashtags = [...hashtags];
+      await this.postRepository.save(post);
     } catch (err) {
       if (err instanceof NotFoundException) {
         throw err;
@@ -168,13 +198,13 @@ export class PostService {
   }
 
   /*
-                              ### 23.03.06
-                              ### 이드보라
-                              ### 포스팅 삭제
-                              */
+                                            ### 23.03.06
+                                            ### 이드보라
+                                            ### 포스팅 삭제
+                                            */
   async deletePost(id: number) {
     try {
-      const result = await this.postRepository.softDelete(id); // soft delete를 시켜주는 것이 핵심입니다!
+      const result = await this.postRepository.softDelete(id);
       if (result.affected === 0) {
         throw new NotFoundException('존재하지 않는 포스트입니다.');
       }
