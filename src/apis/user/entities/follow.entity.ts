cb6796417e05c40713aa0cd4479f181fd8c8f0b6
favorsyoon <favorsyoon@gmail.com>
2023-03-10 21:55:15 +0900

    [fix] npm run start error.

--- src/apis/user/entities/follow.entity.ts
+++ src/apis/user/entities/follow.entity.ts
@@ -1,64 +1,43 @@
-import { Post } from '../../post/entities/post.entity';
-import { PostLike } from '../../post/entities/post-like.entity';
-import { Comment } from '../../comment/entities/comment.entity';
-import {
-  Entity,
-  Column,
-  PrimaryGeneratedColumn,
-  OneToMany,
-  JoinColumn,
-  Unique,
-  CreateDateColumn,
-  UpdateDateColumn,
-  DeleteDateColumn,
-} from 'typeorm';
-import { PostUserTag } from '../../post/entities/post-usertag.entity';
-import { CommentLike } from '../../comment/entities/comment-like.entity';
-import { CommentUserTag } from '../../comment/entities/comment-usertag.entity';
-import { IsEnum } from 'class-validator';
+// import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+// import { User } from './user.entity';
 
-import { Collection } from '../../collection/entities/collection.entity';
+// @Entity()
+// export class Follow {
+//   @PrimaryGeneratedColumn()
+//   id: number;
+
+//   @ManyToOne(type => User, user => user.followings)
+//   following: User;
+
+//   @ManyToOne(type => User, user => user.followers)
+//   follower: User;
+// }
+
+import {
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from './user.entity';
+
+@Unique('following_pair', ['follower', 'following'])
 @Entity()
-@Unique(['nickname'])
-@Unique(['email'])
-export class User {
+export class Follow {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
-  name: string;
+  @JoinColumn({ name: 'follower_id' })
+  @ManyToOne(() => User)
+  follower: User;
 
-  @Column()
-  email: string;
-
-  @Column({ nullable: true })
-  password: string;
-
-  @Column()
-  nickname: string;
-
-  @Column({ nullable: true })
-  phone_number: string;
-
-  @Column({ nullable: true })
-  @IsEnum(['M', 'F'])
-  gender: 'M' | 'F';
-
-  @Column({ nullable: true })
-  birth: Date;
-
-  @Column({ nullable: true, length: 100 })
-  introduce: string;
-
-  //TODO: 회원가입시 default 이미지로값으로 가입완료
-  @Column({ nullable: true, default: 'default-image-url' })
-  profile_image: string;
-
-  @Column({ name: 'follower_count', default: 0 })
-  followerCount: number;
-
-  @Column({ name: 'following_count', default: 0 })
-  followingCount: number;
+  @JoinColumn({ name: 'following_id' })
+  @ManyToOne(() => User)
+  following: User;
 
   @CreateDateColumn({ name: 'created_at' })
   created_at: Date;
@@ -68,32 +47,4 @@ export class User {
 
   @DeleteDateColumn({ name: 'deleted_at' })
   deleted_at: Date;
-
-  @OneToMany((type) => Post, (posts) => posts.user)
-  @JoinColumn()
-  posts: Post[];
-
-  @OneToMany((type) => PostLike, (postLikes) => postLikes.user)
-  @JoinColumn()
-  postLikes: PostLike[];
-
-  @OneToMany((type) => PostUserTag, (postUserTags) => postUserTags.user)
-  @JoinColumn()
-  postUserTags: PostUserTag[];
-
-  @OneToMany((type) => Comment, (comments) => comments.user)
-  @JoinColumn()
-  comments: Comment[];
-
-  @OneToMany((type) => CommentLike, (commentLikes) => commentLikes.user)
-  @JoinColumn()
-  commentLikes: CommentLike[];
-
-  @OneToMany((type) => CommentLike, (commentUserTags) => commentUserTags.user)
-  @JoinColumn()
-  commentUserTags: CommentUserTag[];
-
-  @OneToMany((type) => User, (user) => user.collections)
-  @JoinColumn()
-  collections: Collection[];
 }
