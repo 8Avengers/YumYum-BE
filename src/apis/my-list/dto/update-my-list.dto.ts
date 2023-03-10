0ce1918ab19c0b48743fff8fe120e95573ee8391
sayapin1 <sayapinn1@gmail.com>
2023-03-10 11:37:15 +0900

    fix: 마이리스트 삭제

--- src/apis/my-list/dto/update-my-list.dto.ts
+++
@@ -1,5 +0,0 @@
-import { CreateMyListDto } from './create-my-list.dto';
-import { PartialType } from '@nestjs/mapped-types';
-//create-my-list.dto.ts extendts
-
-export class UpdateMyListDto extends PartialType(CreateMyListDto) {}
