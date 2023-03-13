9795216a3981dfb77bd350ddfcea56cff98fc11b
Admine1147 <117708597+Admine1147@users.noreply.github.com>
2023-03-13 10:46:41 +0900

    #19, #25 feat /레스토랑 수정 삭제 기능 구현 및 entity 수정

--- src/apis/restaurant/dto/create-restaurant.dto.ts
+++ src/apis/restaurant/dto/create-restaurant.dto.ts
@@ -1,24 +1,22 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, IsString } from 'class-validator';
+import { IsString } from 'class-validator';
 
-// @IsString()
-// readonly content: string;
 export class CreateRestaurantDto {
   @ApiProperty({
-    example: '복자씨 연탄구이',
-    description: '가게 이름',
+    example: '제주특별자치도 서귀포시 성산읍 오조리 7-2',
+    description: '지번 주소',
     required: true,
   })
   @IsString()
-  readonly name: string;
+  readonly address_name: string;
 
   @ApiProperty({
-    example: '음식점 > 한식 > 육류,고기',
-    description: '카테고리 이름(카카오)',
+    example: 'FD6',
+    description: '카테고리 코드(카카오)',
     required: true,
   })
   @IsString()
-  readonly category_name: string;
+  readonly category_group_code: string;
 
   @ApiProperty({
     example: '음식점',
@@ -29,21 +27,12 @@ export class CreateRestaurantDto {
   readonly category_group_name: string;
 
   @ApiProperty({
-    example: "'064-782-7330' 이거나 빈값",
-    description: '가게 전화번호',
+    example: '음식점 > 한식 > 육류,고기',
+    description: '카테고리 이름(카카오)',
     required: true,
   })
   @IsString()
-  readonly phone_number: string;
-
-  @ApiProperty({
-    type: 'string[]',
-    format: 'binary',
-    description: '이미지 파일 이름',
-    required: true,
-  })
-  @IsString()
-  readonly img_url: string;
+  readonly category_name: string;
 
   @ApiProperty({
     example: '1896620216',
@@ -54,28 +43,20 @@ export class CreateRestaurantDto {
   readonly kakao_place_id: string;
 
   @ApiProperty({
-    example: '33.4685316070004',
-    description: '위도',
-    required: true,
-  })
-  @IsNumber()
-  readonly latitude: number;
-
-  @ApiProperty({
-    example: '126.921242446619',
-    description: '경도',
-    required: true,
-  })
-  @IsNumber()
-  readonly longitude: number;
-
-  @ApiProperty({
-    example: '제주특별자치도 서귀포시 성산읍 오조리 7-2',
-    description: '지번 주소',
+    example: "'064-782-7330' 이거나 빈값",
+    description: '가게 전화번호',
     required: true,
   })
   @IsString()
-  readonly number_address: string;
+  readonly phone: string;
+
+  @ApiProperty({
+    example: '복자씨 연탄구이',
+    description: '가게 이름',
+    required: true,
+  })
+  @IsString()
+  readonly place_name: string;
 
   @ApiProperty({
     example: '제주특별자치도 서귀포시 성산읍 한도로 124',
@@ -83,5 +64,21 @@ export class CreateRestaurantDto {
     required: true,
   })
   @IsString()
-  readonly road_address: string;
+  readonly road_address_name: string;
+
+  @ApiProperty({
+    example: '126.921242446619',
+    description: '경도',
+    required: true,
+  })
+  @IsString()
+  readonly x: string;
+
+  @ApiProperty({
+    example: '33.4685316070004',
+    description: '위도',
+    required: true,
+  })
+  @IsString()
+  readonly y: string;
 }
--- src/apis/restaurant/restaurant.service.ts
+++ src/apis/restaurant/restaurant.service.ts
@@ -1,7 +1,11 @@
 import { Repository } from 'typeorm/repository/Repository';
 import { Restaurant } from './entities/restaurant.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 @Injectable()
 export class RestaurantService {
   constructor(
@@ -14,41 +18,97 @@ export class RestaurantService {
         ### 최호인  
         ### 음식점 상세정보 가져오기 작성
         */
-  async getRestaurant(id: string) {
+  async getRestaurant(kakao_place_id: string) {
     return await this.restaurantRepository.findBy({
-      kakao_place_id: id,
+      kakao_place_id: kakao_place_id,
     });
   }
 
   createRestaurant(
-    name: string,
-    category_name: string,
+    address_name: string,
+    category_group_code: string,
     category_group_name: string,
-    phone_number: string,
-    img_url: string,
+    category_name: string,
     kakao_place_id: string,
-    latitude: number,
-    longitude: number,
-    number_address: string,
-    road_address: string,
+    phone: string,
+    place_name: string,
+    road_address_name: string,
+    x: string,
+    y: string,
   ) {
     try {
       return this.restaurantRepository.insert({
-        name,
-        category_name,
+        address_name,
+        category_group_code,
         category_group_name,
-        phone_number,
-        img_url,
+        category_name,
         kakao_place_id,
-        latitude,
-        longitude,
-        number_address,
-        road_address,
+        phone,
+        place_name,
+        road_address_name,
+        x,
+        y,
       });
     } catch (err) {
       throw new InternalServerErrorException(
         'Something went wrong while processing your request. Please try again later.',
       );
+    }
+  }
+
+  updateRestaurant(
+    address_name: string,
+    category_group_code: string,
+    category_group_name: string,
+    category_name: string,
+    kakao_place_id: string,
+    phone: string,
+    place_name: string,
+    road_address_name: string,
+    x: string,
+    y: string,
+  ) {
+    try {
+      return this.restaurantRepository.update(
+        { kakao_place_id: kakao_place_id },
+        {
+          address_name,
+          category_group_code,
+          category_group_name,
+          category_name,
+          kakao_place_id,
+          phone,
+          place_name,
+          road_address_name,
+          x,
+          y,
+        },
+      );
+    } catch (err) {
+      throw new InternalServerErrorException(
+        'Something went wrong while processing your request. Please try again later.',
+      );
+    }
+  }
+
+  async deleteRestaurant(kakao_place_id) {
+    try {
+      const result = await this.restaurantRepository.softDelete({
+        kakao_place_id: kakao_place_id,
+      }); // soft delete를 시켜주는 것이 핵심입니다!
+      if (result.affected === 0) {
+        throw new NotFoundException(
+          `Restaurant with kakao_place_id : ${kakao_place_id} not found.`,
+        );
+      }
+    } catch (err) {
+      if (err instanceof NotFoundException) {
+        throw err;
+      } else {
+        throw new InternalServerErrorException(
+          'Something went wrong while processing your request. Please try again later.',
+        );
+      }
     }
   }
 }
