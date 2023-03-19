79fc8d212cc9f852d6e1829777af5b2decb88dc9
sayapin1 <110008118+sayapin1@users.noreply.github.com>
2023-03-20 00:37:52 +0900

    #80 fix: string으로 보내는 files와 multer로 보내는 files를 구분하여 저장하도록 수정.

--- src/apis/post/dto/update-post.dto.ts
+++ src/apis/post/dto/update-post.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePostDto } from './create-post.dto';
+import { IsString, IsArray } from 'class-validator';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {}
+export class UpdatePostDto extends PartialType(CreatePostDto) {
+  @IsArray()
+  @IsString({ each: true })
+  readonly files?: string[];
+}
