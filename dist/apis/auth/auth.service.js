f3a547944755ad6ea86d86787951593a8f32963f
favorsyoon <favorsyoon@gmail.com>
2023-03-22 17:08:02 +0900

    230322 4th

--- dist/apis/auth/auth.service.js
+++ dist/apis/auth/auth.service.js
@@ -26,7 +26,7 @@ let AuthService = class AuthService {
     }
     async oauthLogin(provider, body) {
         const socialService = provider === 'kakao' ? this.socialKaKaoService : this.socialNaverService;
-        console.log('socialService 통과후 body', body, 'socialService 통과후 provider', provider);
+        console.log("socialService 통과후 body", body, "socialService 통과후 provider", provider);
         const token = await socialService.getOauth2Token(body);
         const info = await socialService.getUserInfo(token.access_token);
         console.log('token에는 뭐가 들어가 있을까?', token);
@@ -43,7 +43,7 @@ let AuthService = class AuthService {
             console.log('userEmailFromKakao통과', userEmailFromKakao);
             console.log('userNicknameFromKakao통과', userEmailFromKakao);
             const existingUser = await this.userSignupService.findOne({
-                email: userIdFromKakao,
+                email: userEmailFromKakao,
             });
             if (!existingUser) {
                 user = await this.userSignupService.createOauthUser({
