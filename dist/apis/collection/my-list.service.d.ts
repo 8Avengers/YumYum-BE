0ef990e6e64809be063d97d2c16c131f7f667090
favorsyoon <favorsyoon@gmail.com>
2023-03-24 21:30:44 +0900

    230324 11th 8version

--- dist/apis/collection/my-list.service.d.ts
+++ dist/apis/collection/my-list.service.d.ts
@@ -74,15 +74,6 @@ export declare class MyListService {
     myListPlusPosting(postId: number, collectionId: number[]): Promise<any[]>;
     myListMinusPosting(postId: number, collectionId: number): Promise<void>;
     myListUpdatePosting(postId: number, collectionId: number[]): Promise<void>;
-    HotMyList(): Promise<{
-        id: any;
-        name: any;
-        user: {
-            id: any;
-            nickname: any;
-            profile_image: any;
-        };
-        sumLikes: any;
-    }[]>;
+    HotMyList(): Promise<any>;
     FollowersMyList(userId: number): Promise<CollectionItem[]>;
 }
